ff85ee38438909f1b72ac3071899ebf4fcd91040
amyangfei <amyangfei@gmail.com>
2019-03-27 22:04:29 +0800

    update dm-worker dockerfile

--- dm-worker/run-dm-worker.sh
+++ dm-worker/run-dm-worker.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
-# step.1 replace `source-id` with `${HOSTNAME}`
+#!/bin/bash
 
-# step.2 wait MySQL can accept connection
+mysql_host=${1:-127.0.0.1}
+mysql_port=${2:-3306}
+mysql_user=${3:-root}
+hostname=$(hostname)
+config="conf/dm-worker.toml"
 
-# step.3 run DM-worker
-./dm-worker -config=dm-worker.toml
+# step.1 replace some dynamic variables
+sed -i "s/^source-id = .*$/source-id = \"${hostname}\"/" $config
+sed -i "s/^host = .*$/host = \"${mysql_host}\"/" $config
+sed -i "s/^port = .*$/port = ${mysql_port}/" $config
+sed -i "s/^user = .*$/user = \"${mysql_user}\"/" $config
+if [ "$#" -ge 4 ]; then
+    sed -i "s/^password = .*$/password = \"${4}\"/" $config
+fi
+
+# step.2 run DM-worker
+./bin/dm-worker -config=$config
