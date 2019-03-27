a59e3fc9ce3918f67cbf6a149d287e749ba740ea
csuzhangxc <csuzhangxc@gmail.com>
2019-03-27 17:00:40 +0800

    dm-worker: DM-worker need MySQL startup

--- dm-worker/run-dm-worker.sh
+++ dm-worker/run-dm-worker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # step.1 replace `source-id` with `${HOSTNAME}`
 
-# step.2 run DM-worker
+# step.2 wait MySQL can accept connection
+
+# step.3 run DM-worker
 ./dm-worker -config=dm-worker.toml
