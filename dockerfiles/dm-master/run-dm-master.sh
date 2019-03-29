6398cbc6aa6fd70d655fc528144b2ba2ccaa714a
amyangfei <amyangfei@gmail.com>
2019-03-29 17:46:53 +0800

    fix dm-master config

--- dockerfiles/dm-master/run-dm-master.sh
+++ dockerfiles/dm-master/run-dm-master.sh
@@ -6,7 +6,7 @@ config="conf/dm-master.toml"
 # step.1 add DM-worker deployment config
 for line in $DM_WORKERS; do
     echo "[[deploy]]" >> $config
-    echo $line | awk -F ',' '{print "source_id = \"" $1 "\"\ndm-worker = \"" $2 ":" $3 "\"\n"}' >> $config
+    echo $line | awk -F ',' '{print "source-id = \"" $1 "\"\ndm-worker = \"" $2 ":" $3 "\"\n"}' >> $config
 done
 
 # step.2 run DM-master
