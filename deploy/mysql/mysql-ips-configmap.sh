6e00bfc38b9cc4417d33d49c9eced301b6cbaa35
amyangfei <amyangfei@gmail.com>
2019-03-29 10:53:19 +0800

    update dm-worker deploy config

--- deploy/mysql/mysql-ips-configmap.sh
+++ deploy/mysql/mysql-ips-configmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kubectl get pods -n dm-test -o wide|grep mysql|awk '{print $1, $6}' > MYSQL_IPS.tmp
-kubectl delete configmap -n dm-test mysql-ips-config || ture
+kubectl delete configmap -n dm-test mysql-ips-config || true
 kubectl create configmap -n dm-test mysql-ips-config --from-file=MYSQL_IPS=./MYSQL_IPS.tmp
--- dm-worker/run-dm-worker.sh
+++ dm-worker/run-dm-worker.sh
@@ -5,11 +5,11 @@ config="conf/dm-worker.toml"
 
 # we hack here, each dm-worker-<index> connects to mysql-<index>
 index=${hostname##*-}
-mysql_host=$(grep 'mysql-${index}' $(env MYSQL_IPS) | awk '{print $2}')
+mysql_host=$(echo $MYSQL_IPS | grep "mysql-${index}" | awk '{print $2}')
 
 # step.1 replace some dynamic variables
 sed -i "s/^source-id = .*$/source-id = \"${hostname}\"/" $config
 sed -i "s/^host = .*$/host = \"${mysql_host}\"/" $config
 
 # step.2 run DM-worker
-./bin/dm-worker -config=$config
+./bin/dm-worker -config=$config 2>>"./log/dm-worker-stderr.log"
