83cfa299cadd910ffb7d8ca1f274ab560859cb37
amyangfei <amyangfei@gmail.com>
2019-04-09 16:06:15 +0800

    add data-dam deploy files

---
+++ dockerfiles/bench-client/run-data-dam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hostname=$(hostname)
+config="conf/data-dam.toml"
+
+# we hack here, each data-dam-<index> connects to mysql-<index>
+index=${hostname##*-}
+mysql_host=$(echo $MYSQL_IPS | tr " " "\n" | grep "mysql-${index}," | awk -F ',' '{print $2}')
+
+# step.1 replace some dynamic variables
+sed -i "s/^host = .*$/host = \"${mysql_host}\"/" $config
+
+# step.2 run data-dam
+./bin/data-dam -config=$config 2>>"./log/data-dam-stderr.log"
