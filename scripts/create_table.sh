1cea78bf521105121f178e825f4bdf7e8f060090
amyangfei <amyangfei@gmail.com>
2019-04-09 22:49:33 +0800

    increase test scale

--- scripts/create_table.sh
+++ scripts/create_table.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 default_password=""
-schema="test_1"
-table="t_1"
 namespace="dm-test"
+schemas=( "test_1" "test_2" "test_3" "test_4" )
+tables=( "t_1" "t_2" "t_3" "t_4" "t_5" "t_6" )
 
-read -r -d '' create_table_stmt << EOM
-create table if not exists $schema.$table (id bigint,
+function assemble_sql() {
+    read -r -d '' create_table_stmt << EOM
+create table if not exists $1.$2 (id bigint,
 c1 varchar(60) not null,
 c2 varchar(74) not null,
 c3 datetime not null,
@@ -32,15 +33,21 @@ KEY (\`c12\`),
 UNIQUE (c5, c8, c16)
 );
 EOM
+    echo $create_table_stmt
+}
 
 function create_table() {
-    MYSQL_PWD=$default_password mysql -h $1 -u root -e "create database if not exists $schema"
-    echo $create_table_stmt
-    MYSQL_PWD=$default_password mysql -h $1 -u root -e "$create_table_stmt"
+    MYSQL_PWD=$default_password mysql -h $1 -u root -e "create database if not exists $2"
+    stmt=$(assemble_sql $2 $3)
+    MYSQL_PWD=$default_password mysql -h $1 -u root -e "$stmt"
 }
 
 ips=$(kubectl get pods -n $namespace -o wide|grep -E '^mysql'|awk '{print $6}')
 for ip in $ips; do
     echo $ip
-    create_table $ip
+    for schema in "${schemas[@]}"; do
+        for table in "${tables[@]}"; do
+            create_table $ip $schema $table
+        done
+    done
 done
