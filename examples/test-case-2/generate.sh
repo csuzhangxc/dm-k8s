36cc9c06942eb39bcde159856c10ed091ea4978c
amyangfei <amyangfei@gmail.com>
2019-04-20 17:09:19 +0800

    add test-case-2 scripts

---
+++ examples/test-case-2/generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function assaemble_config() {
+    read -r -d '' line <<- EOF
+  -
+    source-id: "dm-worker-$1"
+    black-white-list:  "instance"
+    mydumper-config-name: "global"
+    loader-config-name: "global"
+    syncer-config-name: "global"
+
+EOF
+    printf '  %s\n\n' "$line"
+}
+
+for i in $(seq 100); do
+    assaemble_config $i
+done
