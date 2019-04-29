a8c8a98fe8792394d982836dfdd42e695895cb8e
amyangfei <amyangfei@gmail.com>
2019-04-29 14:22:49 +0800

    reuse common mysql ips configmap script

--- deploy/mysql/mysql-ips-configmap.sh
+++ deploy/mysql/mysql-ips-configmap.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-ip=$(kubectl get pods -n dm-test-4 -o wide|grep mysql|awk '{print $6}')
-for i in $(seq 0 49); do
-    echo "mysql-$i,$ip" >> MYSQL_IPS.tmp
-done
+kubectl get pods -n dm-test-4 -o wide|grep mysql|awk '{print $1","$6}' > MYSQL_IPS.tmp
 kubectl delete configmap -n dm-test-4 mysql-ips-config || true
 kubectl create configmap -n dm-test-4 mysql-ips-config --from-file=MYSQL_IPS=./MYSQL_IPS.tmp
 rm MYSQL_IPS.tmp
